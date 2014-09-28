b54c08d31e595c847bd5e09b5d94a64527efb8a5
Ubuntu <ubuntu@ip-172-31-19-212.us-west-2.compute.internal>
2014-09-28 19:32:22 +0000

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+   def index
+     # don't need to do anything here 
+     #(too simple of an application). 
+     #Usually will do work here.
+   end
+end
+
